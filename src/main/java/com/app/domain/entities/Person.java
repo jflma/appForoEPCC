34d6cb6ac80fd37be5832d8939c89f6cd6f16bcb
j-h-o-s-e-p <94065071+j-h-o-s-e-p@users.noreply.github.com>
2024-07-18 10:00:14 -0500

    Update Person.java

--- src/main/java/com/app/domain/entities/Person.java
+++ src/main/java/com/app/domain/entities/Person.java
@@ -1,5 +1,40 @@
 package com.app.domain.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
 public class Person {
-    
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(nullable = false)
+  private String firstName;
+  @Column(nullable = false)
+  private String lastName;
+  @Column(nullable = false)
+  private String email;
+  @Column(nullable = false)
+  private String password;
+  public Long getId() {
+    return id;
+  }
+  public String getFirstName() {
+    return first_name;
+  }
+  public String getLastName() {
+    return last_name;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public String getPassword() {
+    return password;
+  }
 }
