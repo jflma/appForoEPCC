e19278d1b0df31b7b6efb8ceae77e045fd4a0923
johanlizma <124108921+johanlizma@users.noreply.github.com>
2024-07-18 04:24:55 -0500

    Create and declare repository AnswerRepositoryImp

---
+++ src/main/java/com/app/repositories/AnswerRepositoryImp.java
@@ -0,0 +1,9 @@
+package com.proyect_v1.mvp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyect_v1.mvp.domain.entities.Answer;
+
+public interface AnswerRepositoryImp extends JpaRepository<Answer,Long>{
+
+}
